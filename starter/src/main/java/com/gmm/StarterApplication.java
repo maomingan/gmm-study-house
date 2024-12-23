a62f1b9d9ff8057caaf5651f5f1946ce532ac86a
ganmaomin <916164663@qq.com>
2024-12-23 18:23:46 +0800

    starter调整

--- starter/src/main/java/com/gmm/StarterApplication.java
+++ starter/src/main/java/com/gmm/StarterApplication.java
@@ -2,13 +2,22 @@ package com.gmm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author Gmm
  * @date 2022/5/20
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan(
+        basePackages = "com.gmm",
+        excludeFilters = @ComponentScan.Filter(
+            type = FilterType.REGEX,
+            pattern = "com\\.gmm\\.mybatis_generator\\..*"
+))
 public class StarterApplication {
 
     public static void main(String[] args) {
